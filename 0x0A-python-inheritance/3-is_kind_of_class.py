aa9bf04e63942d704fe8caa72f27764dc9c360b0
Salvation Makina <salvation.developer@gmail.com>
2023-01-11 18:23:02 +0000

    fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""checks if object is an instance of a class
+or an inherited class
+"""
+
 def is_kind_of_class(obj, a_class):
     """ Function that returns True/False if obj is an
     instance of a_class
