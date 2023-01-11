e4dd20e54e9dcfe305f009fb33dbe73673d9f085
Salvation Makina <salvation.developer@gmail.com>
2023-01-11 18:39:15 +0000

    fix

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
 class BaseGeometry:
-    """ Empty class """
+    """ Base Geometry class """
+
     def area(self):
+        """Method not implemented"""
         raise Exception("area() is not implemented")
