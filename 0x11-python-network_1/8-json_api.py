97e57f95c33b46ed6b25c92c5a6fa8c5e5073dfa
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 19:45:43 +0000

    fix

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A script that takes in a
+- letter and sends a POST request to
+- http://0.0.0.0:5000/search_user with
+- the letter as a parameter."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    payload = {'q': q}
+
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+
+    try:
+        json_data = response.json()
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
