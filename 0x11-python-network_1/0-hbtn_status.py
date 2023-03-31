e8e306e2f187bc9d6333302f446e5ddfddd15341
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 18:26:13 +0000

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
