eddc1f3e7833e2352066897ff896b72bb2f8c66e
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 19:29:46 +0000

    indentation fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that takes in a URL,
 - sends a request to the URL and displays
-- the value of the variable X-Request-Id 
+- the value of the variable X-Request-Id
 - in the response"""
 import requests
 import sys
