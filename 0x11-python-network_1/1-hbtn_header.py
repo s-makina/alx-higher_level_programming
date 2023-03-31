b4182096b4473040ddcf34a8cc693e9f86dee439
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 18:42:13 +0000

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Write a Python script that takes in a URL, 
- - sends a request to the URL and displays the value of the 
- - X-Request-Id variable found in the header of the response.
+"""Takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
-import urllib.request
 import sys
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
