c7d272eb6c11f683dbc617dd6fae096f39adf47c
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 18:51:26 +0000

    fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,6 +16,4 @@ if __name__ == "__main__":
   req = urllib.request.Request(url, data)
 
   with urllib.request.urlopen(req) as response:
-    body = response.read()
-
-  print(body.decode('utf-8'))
+    print(response.read().decode('utf-8')))
