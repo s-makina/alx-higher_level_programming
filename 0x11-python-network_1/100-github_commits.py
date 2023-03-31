6b9e9efda36b3a9aa1608003bd0538602964cf00
Salvation Makina <salvation.developer@gmail.com>
2023-03-31 20:00:46 +0000

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Full Name: {data['full_name']}")
+        print(f"Description: {data['description']}")
+        print(f"Owner: {data['owner']['login']}")
+        print(f"Stars: {data['stargazers_count']}")
+    else:
+        print(f"Error: {response.status_code}")
